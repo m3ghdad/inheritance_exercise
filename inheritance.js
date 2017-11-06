2c2a463f6c596a1a6ffafa772ebaa05a1a917e92
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-06 15:32:34 -0800

    Adds simple inherit function for setting up inheritance using surrogate

--- inheritance.js
+++ inheritance.js
@@ -1,35 +1,49 @@
-Function.prototype.inherits = function() {
-  class Surrogate {
-    constructor(parent, child) {
-      parent = this.parent;
-      child = this.child;
-    }
-  }
+Function.prototype.inherits = function(parent, child) {
+  const Surrogate = function(){};
+  Surrogate.prototype = parent.prototype;
+  child.prototype = new Surrogate();
+  child.prototype.constructor = child;
+}
+
+function MovingObject () {}
+
+function Ship () {}
+Ship.inherits(MovingObject);
+
+function Asteroid () {}
+Asteroid.inherits(MovingObject);
+
+
   //defines a surrogate class
   //Set the prototype of the Surrogate (Surrogate.prototype = SuperClass.prototype)
   //Set Subclass.prototype = new Surrogate()
   //Set Subclass.prototype.constructor = Subclass
-}
-
-function Animal(name) {
-  this.name = name;
-}
-
-Animal.prototype.eat = function() {
-  console.log("mmmm")
-};
-
-function Dog(name) {
-  Animal.call(this, name);
-  // this.name = name;
-};
-
-Dog.prototype = new Surrogate();
-Dog.prototype.constructor = Dog;
-
-Dog.prototype.woof = function() {
-  console.log("woof")
-};
-
-let Surrogate = function(){};
-Surrogate.prototype = Animal.prototype;
+// function Animal(name) {
+//   this.name = name;
+// }
+//
+// Animal.prototype.eat = function() {
+//   console.log("mmmm")
+// };
+//
+// function Dog(name) {
+//   Animal.call(this, name);
+//   // this.name = name;
+// };
+//
+// const inherit = function(parent, child) {
+//   let Surrogate = function(){};
+//   Surrogate.prototype = parent.prototype;
+//   child.prototype = new Surrogate();
+//   child.prototype.constructor = child;
+// }
+//
+// Dog.prototype = new Surrogate();
+// Dog.prototype.constructor = Dog;
+//
+// Dog.prototype.woof = function() {
+//   console.log("woof")
+// };
+//
+// let Surrogate = function(){};
+// Surrogate.prototype = Animal.prototype;
